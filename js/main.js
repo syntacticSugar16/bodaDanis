b539eb54cac094e1963998a212b52398acb08fa7
Mario Alejandro Sánchez Ramón <61639053+syntacticSugar16@users.noreply.github.com>
2025-08-01 13:11:26 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const backgroundMusic = document.getElementById('audio');
+const muteToggleButton = document.getElementById('mute-toggle-button');
+const muteIcon = muteToggleButton.querySelector('.material-icons');
+
 const observer = new IntersectionObserver((entries) =>{
 	entries.forEach((entry) => {
 		console.log(entry);
@@ -37,6 +41,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const letterContainer = document.querySelector('.letter-container');
     const invitacion = document.querySelector('.invitacion');
     const indicacion = document.querySelector('.indicacion');
+    var audio = document.getElementById("audio");
+    const fabContainer = document.querySelector('.fab-container');
+    
 
     envelopeWrapper.addEventListener('click', function() {
         if (!this.classList.contains('open')) {
@@ -59,8 +66,39 @@ document.addEventListener('DOMContentLoaded', function() {
                 letterContainer.style.display = 'none';
                 invitacion.style.position = 'relative';
                 indicacion.style.display = 'none';
+                fabContainer.style.position = 'fixed';
+                audio.play();
             }, 4700);
             
         }
     });
+});
+
+
+
+// Función para actualizar el icono basado en el estado de mute
+function updateMuteIcon() {
+    if (backgroundMusic.muted) {
+        muteIcon.textContent = 'volume_off'; // Mute activado
+    } else {
+        muteIcon.textContent = 'volume_up'; // Mute desactivado
+    }
+}
+
+// Establecer el estado inicial del icono al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+// Asegurarse de que la música esté silenciada al inicio para el autoplay
+    updateMuteIcon();
+
+    // Intentar reproducir la música (puede requerir interacción del usuario en algunos navegadores)
+    backgroundMusic.play().catch(error => {
+        console.log("La reproducción automática de la música fue bloqueada. El usuario deberá interactuar para reproducirla.", error);
+        // Si la reproducción automática es bloqueada, el botón aún funcionará para desmutearla.
+    });
+});
+
+// Añadir el event listener al botón
+muteToggleButton.addEventListener('click', () => {
+    backgroundMusic.muted = !backgroundMusic.muted; // Alternar el estado de mute
+    updateMuteIcon(); // Actualizar el icono
 });
